80b254417ad4386ab777b1ada08f6e8624c88541
Yana <fajviray@gmail.com>
2018-10-17 01:39:03 +0800

    [UPDATE] init response page static view results

--- src/app/pages/responses/search-filters/search-filters.component.ts
+++ src/app/pages/responses/search-filters/search-filters.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search-filters',
-  templateUrl: './search-filters.component.html',
-  styleUrls: ['./search-filters.component.css']
+  selector: 'search-filters',
+  templateUrl: './search-filters.component.pug',
+  styleUrls: ['./search-filters.component.scss']
 })
 export class SearchFiltersComponent implements OnInit {
 
