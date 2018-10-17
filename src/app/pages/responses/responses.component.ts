ae13ca102bed13e8edf68fdbad6f523d01edb956
Yana <fajviray@gmail.com>
2018-10-17 21:16:39 +0800

    [UPDATE] progress search responses integration

--- src/app/pages/responses/responses.component.ts
+++ src/app/pages/responses/responses.component.ts
@@ -9,10 +9,16 @@ export class ResponsesComponent implements OnInit {
 
   questions = ['All', 'Team This Week', 'Amount of Tasks', 'Scrum Today', 'Mental Health', 'Growth Trajectory'];
   answers = ['All', 'Happy', 'Sad', 'No Answer - X', 'No Answer - Idle'];
+  loading = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search(filters) {
+    if (filters instanceof Event) {return;}
+    console.log(filters);
   }
 
 }
--- src/app/pages/responses/search-filters/search-filters.component.ts
+++ src/app/pages/responses/search-filters/search-filters.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'search-filters',
@@ -9,15 +11,15 @@ export class SearchFiltersComponent implements OnInit {
 
   @Input() questions;
   @Input() answers;
+  @Input() loading;
+  @Output() submit = new EventEmitter<object>();
 
-  public daterange: any = {};
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  public options: any = {
+  daterange: any = {
+    start: moment().format('YYYY/MM/DD 00:00:00'),
+    end: moment().format('YYYY/MM/DD 23:59:59')
+  };
+  //daterangepicker options
+  options: any = {
     locale: {
       format: 'MM/DD/YYYY',
       monthNames: [
@@ -40,19 +42,93 @@ export class SearchFiltersComponent implements OnInit {
     autoUpdateInput: true,
     opens: 'left'
   };
+  //dateranges option filters
+  dateOpts = [
+    {id: 1, label: 'Today'},
+    {id: 2, label: 'This Week'},
+    {id: 3, label: 'This Month'},
+    {id: 4, label: 'Date Range'},
+  ]
 
-  public selectedDate(value: any, datepicker?: any) {
-    // this is the date the iser selected
-    console.log(value);
+  openSearchFilter = true;
+  form: FormGroup;
 
+  //selected search filters
+  selectedDateFilter = 1;
+  selectedQuestionsFilter = ['All'];
+  selectedAnswersFilter = ['All'];
+  selectedUserFilter = 'All';
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      date_since: [this.daterange.start, Validators.required],
+      date_until: [this.daterange.end, Validators.required],
+      user: [this.selectedUserFilter, Validators.required],
+      questions: [this.selectedQuestionsFilter, Validators.required],
+      answers: [this.selectedAnswersFilter, Validators.required],
+    });
+  }
+
+  chooseDateRange(option) {
+    this.selectedDateFilter = option;
+    switch(option){
+      case 1: {
+        this.daterange = {
+          start: moment().format('YYYY/MM/DD 00:00:00'),
+          end: moment().format('YYYY/MM/DD 23:59:59')
+        };
+        break;
+      }
+      case 2: {
+        this.daterange = {
+          start: moment().startOf('week').format('YYYY/MM/DD 00:00:00'),
+          end: moment().endOf('week').format('YYYY/MM/DD 23:59:59')
+        };
+        break;
+      }
+      case 3: {
+        this.daterange = {
+          start: moment().startOf('month').format('YYYY/MM/DD 00:00:00'),
+          end: moment().endOf('month').format('YYYY/MM/DD 23:59:59')
+        };
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    this.selectedDate(this.daterange);
+  }
+
+  selectedDate(value: any, datepicker?: any) {
     // any object can be passed to the selected event and it will be passed back here
-    datepicker.start = value.start;
-    datepicker.end = value.end;
+    if(datepicker){
+      datepicker.start = value.start;
+      datepicker.end = value.end;
+    }
 
     // or manupulat your own internal property
     this.daterange.start = value.start;
     this.daterange.end = value.end;
-    this.daterange.label = value.label;
+    this.form.get('date_since').setValue(moment(value.start).format('YYYY/MM/DD 00:00:00'));
+    this.form.get('date_until').setValue(moment(value.end).format('YYYY/MM/DD 23:59:59'));
+  }
+
+  addFilter(arr, value, attr) {
+    arr.push(value)
+    this.form.get(attr).setValue(arr);
+  }
+
+  toggleSearchFilter(){
+    this.openSearchFilter = !this.openSearchFilter;
+  }
+
+  onSubmit(values) {
+    this.loading = true;
+    this.submit.emit(values);
   }
 
 }
