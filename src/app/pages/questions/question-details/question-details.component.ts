582fa3321cfdee2fcbc7f0e9c7c4c9a526ee4ed3
Yana <fajviray@gmail.com>
2018-11-08 07:00:13 +0800

    [UPDATE] questions index and add pages

--- src/app/pages/questions/question-details/question-details.component.ts
+++ src/app/pages/questions/question-details/question-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-question-details',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionDetailsComponent implements OnInit {
 
-  constructor() { }
+  //daterangepicker options
+  datepickeroptions: any = {
+    locale: {
+      format: 'MM/DD/YYYY',
+      monthNames: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ]
+    },
+    alwaysShowCalendars: false,
+    autoApply: true,
+    autoUpdateInput: true,
+    opens: 'left',
+    singleDatePicker: true
+  };
+
+  form: FormGroup;
+  chosenDate = moment().format("D MMMM YYYY");
+  recipientTypes = [
+    { value: 'all', label: 'All' },
+    { value: 'specific', label: 'Specific' }
+  ]
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      'message': ["", Validators.required],
+      'occurence': [1, Validators.required],
+      'trigger_schedule': ["once", Validators.required],
+      'trigger_datetime': ["", Validators.required],
+      'recipients': [""],
+      'chosenDate': [this.chosenDate],
+      'recipientType': [this.recipientTypes[0].value, Validators.required]
+    });
+  }
+
+  selectedDate(value, datepicker){
+    this.chosenDate = value.start.format("D MMMM YYYY");
+    this.form.get('chosenDate').setValue(this.chosenDate);
+
   }
 
 }
--- src/app/pages/questions/questions.module.ts
+++ src/app/pages/questions/questions.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { routing } from './questions.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Daterangepicker } from 'ng2-daterangepicker';
+import { Ng2LoadingSpinnerModule } from 'ng2-loading-spinner';
 
 import { QuestionsComponent } from './questions.component';
 import { QuestionDetailsComponent } from './question-details/question-details.component';
 import { QuestionIndexComponent } from './question-index/question-index.component';
 
+
+import { DateSuffix } from '../../services/pipes/date-suffix.pipe';
+
+
+
 const MODULE_COMPONENTS = [
-  QuestionsComponent
+  QuestionsComponent,
+  QuestionDetailsComponent,
+  QuestionIndexComponent
 ]
 
 @NgModule({
@@ -16,12 +25,13 @@ const MODULE_COMPONENTS = [
     CommonModule,
     routing,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Daterangepicker,
+    Ng2LoadingSpinnerModule
   ],
   declarations: [
     ...MODULE_COMPONENTS,
-    QuestionDetailsComponent,
-    QuestionIndexComponent
+    DateSuffix
   ],
 })
 
