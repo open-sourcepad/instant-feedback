7ec4b3596a278dfdc6c82e84c221285c657a1936
Yana <fajviray@gmail.com>
2018-12-04 01:41:50 +0800

    [UPDATE] traverse talking points

--- src/app/pages/meetings/discuss-multi-form/discuss-multi-form.component.ts
+++ src/app/pages/meetings/discuss-multi-form/discuss-multi-form.component.ts
@@ -1,19 +1,75 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'discuss-multi-form',
   templateUrl: './discuss-multi-form.component.pug',
   styleUrls: ['./discuss-multi-form.component.scss']
 })
-export class DiscussMultiFormComponent implements OnInit {
+export class DiscussMultiFormComponent implements OnInit, OnChanges {
 
   @Input() discussions;
+  @Input() slug_id;
 
   idx: number = 0;
+  action: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activeRoute.queryParams
+      .subscribe(params => {
+        this.action = params['action'];
+        var cur_position = (+this.action);
+        if(this.action != 'start' && cur_position) {
+          if(cur_position >= this.discussions.length){
+            this.idx = (this.discussions.length - 1);
+          }else {
+            this.idx = cur_position - 1;
+          }
+        }else {
+          this.idx = 0;
+        }
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.discussions && !changes.discussions.isFirstChange()){
+      var cur_position = +this.action;
+      if(cur_position){
+        if(cur_position >= this.discussions.length){
+          this.idx = (this.discussions.length - 1);
+          this.changeQuery(this.idx+1);
+        }else {
+          this.idx = cur_position - 1;
+        }
+      }else {
+        this.changeQuery('start');
+      }
+    }
+  }
+
+  nextPoint(){
+    if(this.idx < this.discussions.length){
+      this.idx += 1;
+     }
+  }
+
+  prevPoint(){
+    if(this.idx > 0){
+      this.idx -= 1;
+    }
+  }
+
+  cancel(){
+    this.router.navigateByUrl(`/one-on-ones/${this.slug_id}`);
+  }
+
+  changeQuery(action) {
+    this.router.navigate(['.'], { relativeTo: this.activeRoute, queryParams: {action: action}});
   }
 
 }
