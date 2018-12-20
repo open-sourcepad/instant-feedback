5a925196fd8f59fa507db7346d54199d0673fb86
Yana <fajviray@gmail.com>
2018-12-20 22:46:12 +0800

    [UPDATE] employee give feedback add users

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MeetingService, SessionService, UserService } from '../../services/api';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { User } from '../../models';
 
 
@@ -13,13 +14,24 @@ export class EmployeeComponent implements OnInit {
   currentUser: User;
   sideMenuState: string = 'out';
   feedbackState: string = null;
+  users: object[] = [];
+  loading: boolean = false;
 
   constructor(
-    private session: SessionService
+    private session: SessionService,
+    private userApi: UserService
   ) { }
 
   ngOnInit() {
+    this.loading = true;
     this.currentUser = new User(this.session.getCurrentUser());
+    this.users = this.userApi.query({})
+      .subscribe(res => {
+        this.loading = false;
+        this.users = res['collection']['data'];
+      }, err => {
+        this.loading = false;
+      });
   }
 
   toggleSideMenuState(value) {
