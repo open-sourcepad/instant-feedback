1db7d5e737271c135ab7fbdf514f920b12506ac8
Yana <fajviray@gmail.com>
2018-12-21 23:42:34 +0800

    [SERVICE] feedback api

--- src/app/services/api/index.ts
+++ src/app/services/api/index.ts
@@ -2,6 +2,7 @@ export { PulseService } from './pulse.service';
 export { ActionItemService } from './action-item.service';
 export { AnswerService } from './answer.service';
 export { DiscussionService } from './discussion.service';
+export { FeedbackService } from './feedback.service';
 export { QuestionService } from './question.service';
 export { UserService } from './user.service';
 export { MeetingService } from './meeting.service';
