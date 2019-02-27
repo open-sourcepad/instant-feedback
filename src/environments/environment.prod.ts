54edc22f47ce949b8bfd3cb3f888187dbb0d2e4c
Yana <fajviray@gmail.com>
2019-02-27 17:57:53 +0800

    [CONFIG] deploy prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  api_url: "https://instant-feedback-api.spstage.com"
+  api_url: "https://if-api.spsuite.co"
 };
-
-// export const environment = {
-//   api_url: "https://api.spsuite.co",
-//   base_url: 'https://spsuite.co',
-//   production: true,
-//   google_client: '42075831752-9jf638265r2pirv7m31h141dnpf4hkas.apps.googleusercontent.com'
-// };
