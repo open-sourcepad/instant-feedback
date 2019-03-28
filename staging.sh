9a176ff46183dbaeca92d63bfacab7b2f164cbdf
Kay Pambid <feliciakaye.pambid@gmail.com>
2019-03-28 20:40:56 +0800

    modify staging.sh

--- staging.sh
+++ staging.sh
@@ -1,2 +1,2 @@
-[ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] &&
-eval ruby deploy/staging.rb  || echo "Make sure your are in sync with master"
+[ "`git log --pretty=%H ...refs/heads/staging^ | head -n 1`" = "`git ls-remote origin -h refs/heads/staging |cut -f1`" ] &&
+eval ruby deploy/staging.rb  || echo "Make sure your are in sync with staging"
