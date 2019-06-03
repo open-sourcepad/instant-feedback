b792a344b8909568ca06a33ae4be2e2834e24bf0
Adrian <adriana@Adrians-MacBook-Pro-6.local>
2019-06-03 23:45:59 +0800

    Fix moment timezone default

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import * as moment from 'moment';
-import 'moment-timezone';
+import * as moment from 'moment-timezone';
 
 if (environment.production) {
   enableProdMode();
